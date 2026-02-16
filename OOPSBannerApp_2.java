c9acde7a5549ca238f320714fa57c6e40e84cb6d
omrajput-jsr <omrajputjsr@gmail.com>
2026-02-16 15:41:52 +0530

    Coded to Render OOPS as Banner using Print Statements

---
+++ OOPSBannerApp_2.java
@@ -0,0 +1,20 @@
+/**
+ * @author: Om Rajput
+ * @version: 1.0
+ */
+
+public class OOPSBannerApp_2 {
+    public static void main(String[] args) {
+
+        System.out.println(" *****     *****    ******     *****  ");
+        System.out.println("*     *   *     *   *     *   *     * ");
+        System.out.println("*     *   *     *   *     *   *       ");
+        System.out.println("*     *   *     *   ******     *****  ");
+        System.out.println("*     *   *     *   *              *  ");
+        System.out.println("*     *   *     *   *        *     *  ");
+        System.out.println(" *****     *****    *         *****   ");
+
+    }
+}
+
+
